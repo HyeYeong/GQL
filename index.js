79ea19d85691f253cf9a14bb4f19d4e6ec9188e8
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-07-12 18:11:51 +0900

    feat: example qpl

--- index.js
+++ index.js
@@ -1,8 +1,72 @@
-import {
-  GraphQLServer
-} from "graphql-yoga";
-const server = new GraphQLServer({
-  typeDefs: "graphql/schema.graphql"
-})
+const express = require('express'); // npm install express
+const app = express();
+const port = 3010;
+const path = require('path');
+const { buildSchema } = require('graphql'); // npm install graphql
+var { graphqlHTTP } = require('express-graphql'); // npm install express-graphql
 
-server.start(() => console.log("GraphQl server is running!"))
+const schema = buildSchema(`
+    type Topic{
+        id:Int!
+        title:String!
+        body:String
+    }
+    type Query {
+        title:String
+        topics:[Topic]
+        getTopic(id: Int!):Topic
+    }
+    type Mutation {
+        createTopic(title: String!, body: String): Topic 
+    }
+`)
+
+const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'}
+]
+
+const getTopic = function(args){
+    const id = args.id;
+    const topic = topics.find(topic=>topic.id === id);
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        return topics[i]
+      }
+    }
+    return null;
+}
+
+let nextId = 4;
+const createTopic = function(args){
+    const newTopic = {
+        id:nextId,
+        title:args.title,
+        body:args.body
+    };
+    topics.push(newTopic);
+    nextId = nextId + 1;
+    return newTopic;
+}
+
+var root = {
+    title:'egoing blog',
+    topics:topics,
+    getTopic:getTopic,
+    createTopic:createTopic
+}
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
